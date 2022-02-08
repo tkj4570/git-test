bc154820b85371f1f0d012315ccc74bb0130fa9c
tkj4570 <457066709@qq.com>
2022-02-08 09:27:40 +0800

    second commit

--- src/main/java/com/xiana/git/Main.java
+++ src/main/java/com/xiana/git/Main.java
@@ -12,5 +12,6 @@ package com.xiana.git;
 public class Main {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git2!");
     }
 }
