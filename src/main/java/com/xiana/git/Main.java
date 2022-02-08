9e6b414c4b6006b748d5c6e337c137ce3cbf8bc9
tkj4570 <457066709@qq.com>
2022-02-08 09:23:20 +0800

    first commit

---
+++ src/main/java/com/xiana/git/Main.java
@@ -0,0 +1,16 @@
+package com.xiana.git;
+
+/**
+ * 包名称: com.xiana.git
+ * 项目名称: git-test
+ * 描述: [类型描述]
+ * 创建时间: 2022/2/8 上午9:22
+ *
+ * @author <a href="mail to: 457066709@qq.com" rel="nofollow">唐科技</a>
+ * @version v1.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
