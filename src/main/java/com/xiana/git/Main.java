2a98ff0ed1e37b4ff48cf0fa661572ea05daba1f
tkj4570 <457066709@qq.com>
2022-02-08 14:33:11 +0800

    hot-fix second commit

--- src/main/java/com/xiana/git/Main.java
+++ src/main/java/com/xiana/git/Main.java
@@ -14,5 +14,6 @@ public class Main {
         System.out.println("hello git!");
         System.out.println("hello git2!");
         System.out.println("hello git3!");
+        System.out.println("hot-fix test!");
     }
 }
